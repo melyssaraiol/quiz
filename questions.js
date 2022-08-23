49da3cfa3ad81d241ea050da8a521959e5dce398
melyssaraiol <melyssaraiol@gmai.com>
2022-08-23 09:57:26 -0300

    first commit

---
+++ questions.js
@@ -0,0 +1,101 @@
+let questions = [
+    {
+        question: 'A função getElementsByClassName(), retorna que tipo de conteúdo?',
+        options: [
+            'Retorna um JSON contendo as informções dos elementos.',
+            'Retorna um único elemento.',
+            'Retorna um array de elementos que contém a classe informada no atributo class.'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Usamos try/catch para:',
+        options: [
+            'Podemos usar essa estrutura quando queremos acessar elementos nativos do navegador.',
+            'Tratar instruções que possam gerar erros, e assim, lançar exceções.',
+            'Funciona como o if/else, verifica se uma operação foi executada.'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Qual a sintaxe correta para adicionar um arquivo javascript externo na tag script?',
+        options: [
+            'name="xxx.js"',
+            'src="xxx.js"',
+            'href="xxx.js"'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Para utilizar um "debugador" disponível no browser, usamos:',
+        options: [
+            'debugger;',
+            'debug;',
+            'pause break;'
+        ],
+        answer: 0
+    },
+    {
+        question: 'Qual definição de class  abaixo está correta?',
+        options: [
+            'public class Carro{}',
+            'class Carro({});',
+            'class Carro{}'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Para requisitar uma informação usando o protocolo GET, podemos usar:',
+        options: [
+            "request_uri('url');",
+            "get('url');",
+            "fetch('url')"
+        ],
+        answer: 2
+    },
+    {
+        question: 'Para obtermos o valor de um atributo de um elemento, usamos:',
+        options: [
+            "element.getAttribute('atributo');",
+            "element.get('atributo');",
+            "getAttribute(element, 'atributo');"
+        ],
+        answer: 0
+    },
+    {
+        question: 'Qual dos códigos a baixos foram introduzidos no Ecma 6?',
+        options: [
+            'var',
+            'let',
+            'function(){}'
+        ],
+        answer: 1
+    },
+    {
+        question: 'O valor de uma constante pode ser mudado após a sua declaração?',
+        options: [
+            'Não, uma vez declarado o valor não pode ser modificado',
+            'Sim, uma constante pode ter seu valor mutável.',
+            'Sim, desde que a constante não tenha sido utilizada.'
+        ],
+        answer: 0
+    },
+    {
+        question: 'Como o loop while começa?',
+        options: [
+            'while (i <= 10)',
+            'while (i <= 10; i++)',
+            'while i = 1 to 10'
+        ],
+        answer: 0
+    },
+    {
+        question: 'Gostou do projeto?',
+        options: [
+            'Sim',
+            'Não',
+            'Talvez'
+        ],
+        answer: 0
+    },
+];
